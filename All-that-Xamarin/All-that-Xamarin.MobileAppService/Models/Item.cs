d6745c48b4be89a41fa2a2da076136c5e688a7fe
Jiyoung Lim <jylim@monitoring6.com>
2020-01-22 10:29:44 +0900

    Add project files.

---
+++ All-that-Xamarin/All-that-Xamarin.MobileAppService/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace All_that_Xamarin.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ All-that-Xamarin/All-that-Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using All_that_Xamarin.Models;
+
+namespace All_that_Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
