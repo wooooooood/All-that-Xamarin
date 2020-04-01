13e858a90d1476339916e1ae911800718971d570
wooooooood <13pft13@gmail.com>
2020-04-01 18:24:22 +0900

    Explore page (+Map)

--- All-that-Xamarin/All-that-Xamarin/Views/ExplorePage.xaml.cs
+++ All-that-Xamarin/All-that-Xamarin/Views/ExplorePage.xaml.cs
@@ -1,20 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AllThatXamarin.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class ExplorePage : ContentPage
-{
-    public ExplorePage()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExplorePage : ContentPage
     {
-        InitializeComponent();
+        public ExplorePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnOpenMapButton_Clicked(object sender, EventArgs e)
+        {
+            var placemark = new Placemark
+            {
+                CountryName = "United States",
+                AdminArea = "WA",
+                Thoroughfare = "Microsoft Building 25",
+                Locality = "Redmond"
+            };
+            var options = new MapLaunchOptions
+            {
+                Name = "Microsoft Building 25",
+                NavigationMode = NavigationMode.Driving
+            };
+
+            await Map.OpenAsync(placemark, options);
+        }
     }
-}
 }
